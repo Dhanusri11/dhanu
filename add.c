5d1aecae3ca0b062fb2f62fae21b54b2533972c2
Dhanusri11 <dhanusri1130@gmail.com>
2025-05-13 09:30:31 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+     
+     int x,y;
+     x=12;
+     y=20;
+     
+   
+     printf("%d\n",x+y);
+     
+    return 0;
+}
---
+++ swap.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main() {
+     
+     int x,y,z,sum;
+     x=12;
+     y=20;
+     z=1;
+   
+     printf("%d\n",x+y+z);
+     
+    return 0;
+}
