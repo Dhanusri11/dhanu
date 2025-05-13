b05f98f7ae996f4270bec97cf9ece53150be2b08
Dhanusri11 <dhanusri1130@gmail.com>
2025-05-13 12:02:34 +0530

    Add files via upload

---
+++ formating.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("%.4f",5.0/-3);
+    
+}
+    
---
+++ string.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    char arr[20]="happu";
+    char x[6]={'H','A','P','P','Y'};
+    printf("\n%s\n",arr);
+    printf("%s",x);
+
+    return 0;
+}
