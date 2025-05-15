e31986319a2cf7ecfbf1957885b487764fae862b
Dhanusri11 <dhanusri1130@gmail.com>
2025-05-15 11:12:06 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <limits.h>
+int main() {
+    int num;
+    scanf("%d",&num);
+    switch(num){
+        case 1:
+         printf("January");
+         break;
+        case 2:
+         printf("tues");
+         break;
+        case 3:
+         printf("wed");
+         break; 
+        case 4:
+         printf("mon");
+         break;
+        case 5:
+         printf("mon");
+         break;
+    }
+    return 0;
+}
